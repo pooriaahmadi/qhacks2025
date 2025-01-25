b4ff2140b5fd3de4d31e96dfb20f9eabdb60903d
pooriaahmadi <pooriababak444@gmail.com>
2025-01-25 11:28:23 -0500

    updated the toggles

--- extension/sidepanel/popup.js
+++ extension/sidepanel/popup.js
@@ -1,7 +1,10 @@
-const cards = document.querySelectorAll('.card');
+const cards = document.querySelectorAll(".card");
 
 cards.forEach((card) => {
-  card.addEventListener('click', () => {
-    card.classList.toggle('open');
+  card.addEventListener("click", () => {
+    cards.forEach((c) => {
+      c.classList.remove("open");
+    });
+    card.classList.toggle("open");
   });
 });
